348379f074ea7818a6d9856a74037a388eead8b9
Vivek Gogi <vivekgogi123vg@gmail.com>
2024-05-17 02:41:00 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,31 @@
+// import mongoose from 'mongoose';
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    console.log('MongoDB URI:', process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
